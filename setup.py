f6df7bc3bf273681e85f1f2a9811184920030610
Chad Lucas <cjlucas85@gmail.com>
2018-07-21 17:34:56 -0700

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# from distutils.core import setup
+from setuptools import setup, find_packages
+setup(
+    name='terse',
+    version='0.0.1',
+    description='Collection of useful decorators for keeping code terse.',
+    author='Chad Lucas',
+    author_email='cjlucas85@gmail.com',
+    url='None',
+    packages=find_packages(),
+    # packages=['distutils', 'distutils.command'],
+)
